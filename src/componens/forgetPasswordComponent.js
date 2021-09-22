ec233017f60664480e224829c1faac6bc8408cf3
JRSKelvin <74870436+JRSKelvin@users.noreply.github.com>
2021-09-22 15:49:17 +0700

    Tambahkan Forget Password Page

---
+++ src/componens/forgetPasswordComponent.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import mainLogo from '../pictures/Glints K-6-logos_black.png'
+
+const forgetPasswordComponent = () => {
+    return (
+        <div className="containerForget">
+            <div className="formContainer-Forget">
+                <form action="#" onsubmit="return false">
+                    <img src={mainLogo} alt="" />
+                    <h1>Forget Password</h1>
+                    Type your email and new password here!
+                    <input type="email" placeholder="Email" id="email" style={{marginTop:'25px'}} />
+                    <input type="password" placeholder="New Password" id="pass" />
+                    <button type="submit" onclick="Forget()">Reset Password</button>
+                    <a href="./index.html">Back to Sign In?</a>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default forgetPasswordComponent
---
+++ src/componens/login/forget.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ForgetPassword from '../../componens/forgetPasswordComponent';
+import './style.css'
+
+const forget = () => {
+    return (
+        <div>
+            <ForgetPassword />
+        </div>
+    )
+}
+
+export default forget
