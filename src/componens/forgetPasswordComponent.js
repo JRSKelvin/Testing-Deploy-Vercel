102685ccc81bed4749f21a3dc2ca7ae14bd333d6
JRSKelvin <74870436+JRSKelvin@users.noreply.github.com>
2021-09-24 14:13:59 +0700

    Installed Sweet Alert, Perbaikan Style.css & Pindah Function PasswordComponent.JS ke Forget.JS

--- src/componens/forgetPasswordComponent.js
+++ src/componens/forgetPasswordComponent.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import mainLogo from '../pictures/Glints K-6-logos_black.png'
 import axios from 'axios'
+import Swal from 'sweetalert2'
+import { Forget2 } from './login/forget'
 
 const forgetPasswordComponent = () => {
     return (
@@ -12,107 +14,13 @@ const forgetPasswordComponent = () => {
                     Type your email and new password here!
                     <input type="email" placeholder="Email" id="email" style={{marginTop:'25px'}} />
                     <input type="password" placeholder="New Password" id="pass" />
-                    <input type="text" placeholder="Answer Question Recovery" id="answer" />
-                    <button type="submit" onClick={Forget}>Reset Password</button>
+                    <input type="text" placeholder="Answer Question Recovery (What Is Your Hobby)" id="answer" />
+                    <button type="submit" onClick={Forget2}>Reset Password</button>
                     <a href="/">Back to Sign In?</a>
                 </form>
             </div>
         </div>
     )
 }
-
-function Forget() {
-  if (Forget) {
-    let emailInput = document.getElementById("email").value;
-    let passInput = document.getElementById("pass").value;
-    let answerInput = document.getElementById("answer").value;
-
-    axios.get(
-        "https://613618d38700c50017ef53e3.mockapi.io/UserAdmin"
-      )
-      .then((response) => {
-        console.log(response.data);
-        const myData = response.data;
-        let user = myData.filter((item) => item.email === emailInput);
-        
-        console.log(user);
-        if (user.length > 0) {
-          if (user[0].email === emailInput) {
-            if (user[0].answerrecovery === answerInput){
-              if (passInput === "" || passInput === null) {
-                alert("Harap memasukkan password");
-                return false;
-              } else {
-                if (passInput.length < 6) {
-                  alert("Password minimal 6 karakter");
-                  document.getElementById("pass").value = "";
-                  return false;
-                }
-              }
-              const getUserID = user[0].id;
-              const changePassword = { password: passInput };
-              console.log(getUserID, changePassword);
-
-              axios.put(
-                "https://613618d38700c50017ef53e3.mockapi.io/UserAdmin/" +
-                  getUserID,
-                changePassword
-              );
-
-              user[0].password = passInput;
-              alert("Password Berhasil Diganti, Silahkan Login Kembali");
-              console.clear();
-              window.location.href ="/"
-            }else {
-              alert("Email Tidak Ditemukan Atau Jawaban Recovery Salah");
-              console.clear();
-            }
-          }
-        } else {
-          alert("Email Tidak Ditemukan Atau Jawaban Recovery Salah");
-          console.clear();
-        }
-      })
-      
-
-
-    // fetch("https://613618d38700c50017ef53e3.mockapi.io/UserAdmin")
-    //   .then((response) => response.json())
-    //   .then((result) => {
-    //     let user = result.filter((item) => item.email === emailInput);
-    //     console.log(user);
-    //     if (user.length > 0) {
-    //       if (user[0].email === emailInput) {
-    //         if (passInput === "" || passInput === null) {
-    //           alert("Harap memasukkan password");
-    //           return false;
-    //         } else {
-    //           if (passInput.length < 6) {
-    //             alert("Password minimal 6 karakter");
-    //             document.getElementById("pass").value = "";
-    //             return false;
-    //           }
-    //         }
-    //         const getUserID = user[0].id;
-    //         const changePassword = { password: passInput };
-    //         console.log(getUserID, changePassword);
-
-    //         axios.put(
-    //           "https://613618d38700c50017ef53e3.mockapi.io/UserAdmin/" +
-    //             getUserID,
-    //           changePassword
-    //         );
-
-    //         user[0].password = passInput;
-    //         alert("Password Sudah Diganti");
-    //         //console.clear();
-    //       }
-    //     } else {
-    //       alert("Email tidak ditemukan");
-    //     }
-    //   });
-  }
-}
-
 
 export default forgetPasswordComponent
--- src/componens/login/forget.js
+++ src/componens/login/forget.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ForgetPassword from '../forgetPasswordComponent';
 import './style.css'
+import axios from 'axios'
+import Swal from 'sweetalert2'
+export const Forget2 = () => {
+    Forget()
+}
 
 const forget = () => {
     return (
@@ -8,6 +13,127 @@ const forget = () => {
             <ForgetPassword />
         </div>
     )
+}
+
+function Forget() {
+    if (Forget) {
+      let emailInput = document.getElementById("email").value;
+      let passInput = document.getElementById("pass").value;
+      let answerInput = document.getElementById("answer").value;
+  
+      axios.get(
+          "https://613618d38700c50017ef53e3.mockapi.io/UserAdmin"
+        )
+        .then((response) => {
+          console.log(response.data);
+          const myData = response.data;
+          let user = myData.filter((item) => item.email === emailInput);
+          
+          console.log(user);
+          if (user.length > 0) {
+            if (user[0].email === emailInput) {
+              if (user[0].answerrecovery === answerInput){
+                if (passInput === "" || passInput === null) {
+                  //alert("Harap memasukkan password");
+                  Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Harap Memasukkan Password'
+                  })
+                  return false;
+                } else {
+                  if (passInput.length < 6) {
+                    //alert("Password minimal 6 karakter");
+                    Swal.fire({
+                      icon: 'error',
+                      title: 'Oops...',
+                      text: 'Password Minimal 6 Karakter'
+                    })
+                    document.getElementById("pass").value = "";
+                    return false;
+                  }
+                }
+                const getUserID = user[0].id;
+                const changePassword = { password: passInput };
+                console.log(getUserID, changePassword);
+  
+                axios.put(
+                  "https://613618d38700c50017ef53e3.mockapi.io/UserAdmin/" +
+                    getUserID,
+                  changePassword
+                );
+  
+                user[0].password = passInput;
+                //alert("Password Berhasil Diganti, Silahkan Login Kembali");
+                Swal.fire({
+                  title: 'Berhasil Ganti',
+                  text: 'Password Berhasil Diganti, Silahkan Login Kembali',
+                  icon: 'success',
+                  timer: 2000
+                }).then(function() {
+                  window.location = "/";
+              });
+                console.clear();
+                //window.location.href ="/"
+              }else {
+                //alert("Email Tidak Ditemukan Atau Jawaban Recovery Salah");
+                Swal.fire({
+                  icon: 'error',
+                  title: 'Oops...',
+                  text: 'Email Tidak Ditemukan Atau Jawaban Recovery Salah'
+                })
+                console.clear();
+              }
+            }
+          } else {
+            //alert("Email Tidak Ditemukan Atau Jawaban Recovery Salah");
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: 'Email Tidak Ditemukan Atau Jawaban Recovery Salah'
+            })
+            console.clear();
+          }
+        })
+        
+  
+  
+      // fetch("https://613618d38700c50017ef53e3.mockapi.io/UserAdmin")
+      //   .then((response) => response.json())
+      //   .then((result) => {
+      //     let user = result.filter((item) => item.email === emailInput);
+      //     console.log(user);
+      //     if (user.length > 0) {
+      //       if (user[0].email === emailInput) {
+      //         if (passInput === "" || passInput === null) {
+      //           alert("Harap memasukkan password");
+      //           return false;
+      //         } else {
+      //           if (passInput.length < 6) {
+      //             alert("Password minimal 6 karakter");
+      //             document.getElementById("pass").value = "";
+      //             return false;
+      //           }
+      //         }
+      //         const getUserID = user[0].id;
+      //         const changePassword = { password: passInput };
+      //         console.log(getUserID, changePassword);
+  
+      //         axios.put(
+      //           "https://613618d38700c50017ef53e3.mockapi.io/UserAdmin/" +
+      //             getUserID,
+      //           changePassword
+      //         );
+  
+      //         user[0].password = passInput;
+      //         alert("Password Sudah Diganti");
+      //         //console.clear();
+      //       }
+      //     } else {
+      //       alert("Email tidak ditemukan");
+      //     }
+      //   });
+    }
 }
 
 export default forget
